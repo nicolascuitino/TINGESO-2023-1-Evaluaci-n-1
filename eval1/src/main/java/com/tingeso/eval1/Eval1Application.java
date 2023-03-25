8de5dc3d973ddfe4c0b22d1342203b79e0512893
nicolascuitino <84163512+nicolascuitino@users.noreply.github.com>
2023-03-25 19:32:11 -0300

    Creacion proyecto Spring boot

---
+++ eval1/src/main/java/com/tingeso/eval1/Eval1Application.java
@@ -0,0 +1,13 @@
+package com.tingeso.eval1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eval1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eval1Application.class, args);
+	}
+
+}
